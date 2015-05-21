04095e200cacea852c07ad148841a117b80fa3c2
Pankaj Sharma <panxshaz@My-Precious-13.local>
2015-05-21 20:59:17 +0530

    Initial Commit

---
+++ FranklyMeTask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FranklyMeTask
+//
+//  Created by Pankaj Sharma on 21/05/15.
+//  Copyright (c) 2015 Pankaj Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FranklyMeTask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FranklyMeTask
+//
+//  Created by Pankaj Sharma on 21/05/15.
+//  Copyright (c) 2015 Pankaj Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
